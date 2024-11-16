7da83655011ece352291a634cebb3d5f59902c53
Darshan <darshanc4696@gmail.com>
2024-11-16 20:42:34 +0530

    spring security removed

--- src/main/java/com/dquiz/quizservices/UserService.java
+++ src/main/java/com/dquiz/quizservices/UserService.java
@@ -1,7 +1,6 @@
 package com.dquiz.quizservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.dquiz.models.User;
@@ -12,10 +11,8 @@ public class UserService {
 	
 	@Autowired
 	private UserRepo userRepo;
-	private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(7);
 	
 	public void saveUser(User user) {
-		user.setPassword(encoder.encode(user.getPassword()));
 		userRepo.save(user) ;
 		
 	}
@@ -25,7 +22,7 @@ public class UserService {
 	    User user = userRepo.findByEmail(email);
 	    
 	    if (user != null) {
-	        if (encoder.matches(password, user.getPassword())) {
+	        if (password.equals(user.getPassword())) {
 	            return "loggedin";
 	        } else {
 	            return "password wrong";
